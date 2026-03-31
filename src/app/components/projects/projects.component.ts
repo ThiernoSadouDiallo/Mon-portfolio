79d52087bb0bf80f443a6507388bf6d4f69b5f23
Thierno Sadou Diallo <thiernosadoudiallo356@gmail.com>
2026-03-31 19:42:09 +0200

    Fix syntax error and add new project details

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -30,11 +30,17 @@ export class ProjectsComponent {
       description: 'Site portfolio moderne et responsive avec animations et transitions fluides.',
       technologies: ['Angular', 'TypeScript', 'Html', 'Css'],
       github: '#'
-    }
+    },
     {
       title: 'Jeu de PUZZLE',
       description: 'Le but de ce projet est de réaliser une application de jeu, dotée d\'une interface graphique, mais pouvant être utilisé sans l\'interface graphique tout en implémentant la vue console, ce jeu consiste en un puzzle à glissière.',
       technologies: ['Angular', 'TypeScript', 'Html', 'Css'],
+      github: '#'
+    },
+    {
+      title: 'Jukebox Numérique',
+      description: 'Le but du projet est de réaliser une application de musique qui sera nommée MUSITY, dotée d\'une interface graphique. L\'objectif est de remplacer et valoriser l\'ancien jukebox qui existait dans les bars en France pour apporter une meilleur expérience clientèle.',
+      technologies: ['flutter', 'FlutterFlow'],
       github: '#'
     }
   ];
